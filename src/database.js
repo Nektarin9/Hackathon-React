e46ee3b08402aee543684bde25ef3df93ee7a907
Kulibars <125401672+Kulibars@users.noreply.github.com>
2024-05-18 22:47:10 +0300

    Добавил данные

--- src/database.js
+++ src/database.js
@@ -97,52 +97,62 @@ export const database = {
 	/*-----------------------------------------------------------*/
 	DB_MICHAEL: {
 		name: 'Michael',
+		age: 33,
+		city: 'Москва',
 		technology: [
-			{ protsent: '70%', name: 'HTML', color: '#ac1c02' },
-			{ protsent: '73%', name: 'СSS', color: '#1f406b' },
-			{ protsent: '13%', name: 'SСSS', color: '#d10073' },
-			{ protsent: '57%', name: 'JS', color: '#cdb100' },
+			{ protsent: '80%', name: 'HTML', color: '#ac1c02' },
+			{ protsent: '70%', name: 'СSS', color: '#1f406b' },
+			{ protsent: '10%', name: 'SСSS', color: '#d10073' },
+			{ protsent: '80%', name: 'JS', color: '#cdb100' },
 			{
-				protsent: '62%',
+				protsent: '60%',
 				name: 'REACT (Redux, React Redux, React-router)',
 				color: '#005aa9',
 			},
-			{ protsent: '46%', name: 'GIT', color: 'black' },
-			{ protsent: '22%', name: 'PostgeSQL', color: '#197547' },
+			{
+				protsent: '30%',
+				name: 'REACT-NATIVE',
+				color: '#005aa9',
+			},
+			{ protsent: '50%', name: 'GIT', color: 'black' },
+			{ protsent: '20%', name: 'Python', color: '#197547' },
+			{ protsent: '10%', name: 'Django', color: '#197547' },
 		],
 		badge: [
 			{
-				title: 'Team lead',
-				color: '#cdb100',
+				title: 'Developer',
+				color: 'rgb(141 76 163)',
 			},
 			{
-				title: 'Инженер',
+				title: 'React',
+				color: '#1f406b',
+			},
+			{
+				title: 'Frontend',
 				color: '#1f406b',
 			},
 		],
 
 		sliderImg: [
-			'https://iimg.su/s/17/wd8ir1YgV0J289DhX1RUF9jAwHHkNPCe3AsaqDfG.png',
-			'https://iimg.su/s/17/6iN0Lsr8M53zaC3aVjiMf6q3p1ALe3goZj7pBl5e.png',
-			'https://iimg.su/s/17/4o4Sn2RAnP2ohh412s3RXP4VwLlQ0q5FBMtHzIsv.png',
-			'https://iimg.su/s/17/bLMT5kUE1kgNzfYnfjI5r6btFDqfkQurjhTwT1HF.png',
-			'https://iimg.su/s/17/HgZE4LIk5fQuc2OJxIKZ7QHjum9WkVetM2WG95Jc.png',
-			'https://iimg.su/s/17/ADjPAc3L3n6SpaGK9UWXWbaJprYoHV0umGLh77Lm.png',
+			'https://iimg.su/s/18/th_kqISbbXuRlO9i2RskYGtReqUzg75AqXuIGemkVd7.jpg',
+			'https://iimg.su/s/18/83bnStzW8SndsjtiLy3fNY4XDSCv6vgdzQsKIm0k.jpg',
+			'https://iimg.su/s/18/FFfoArUnwzSnmJNFi0o2GK7r7t7Pid63wum7HG0A.jpg',
+			'https://iimg.su/s/18/3yzhOYkzy62PqeIdc9mTyD5620Bb76NCL1lv1tJY.jpg',
 		],
 		person: {
 			name: 'Michael',
-			age: 27,
+			age: 33,
 			city: 'Москва',
-			photo: 'https://iimg.su/s/17/zfO2xboHrMegkLvb09FWfTHdFYDlX3HioHYUTFj8.png',
-			content: `Я ведущий инженер и Frontend разработчик, закончил Московский технический университет связи и информатики.
-			Отлично владею HTML, CSS, JS, React, имею опыт коммерческой разработки, неоднократно учавствовал в хакатона в качестве Team Lead.
-			Имею опыт работы с коммутаторами: Qtech, D-link, Edge Core и другими. мне нравятся ночные и вечерные прогулки. В свободное время я люблю посещать спортзал.`,
+			photo: 'https://iimg.su/s/18/sWx3HStAIZSyOQxoeZxblSKZlOVA4DCSU1mfyNxd.jpg',
+			content: `Активно учусь на Frontend разработчика. Родился в Москве, но на данный момент живу в городе Гюмри (Республика Армения).
+			Неплохо владею HTML, CSS и JS, а так же React и React Native.
+			Немного знаю Python (Написал Telegram бота и простенькую игру)
+			Увлекаюсь Спелестологией, фотографией и историей, люблю читать и путешествовать, открывать для себя новое`,
 		},
 		socialNetwork: {
-			vk: 'https://vk.com/id88581034',
-			telegram: 'https://t.me/NektarinKoO',
-			github: 'https://github.com/Nektarin9',
+			vk: 'https://vk.com/id24192203',
+			telegram: 'https://t.me/Kulibars',
+			github: 'https://github.com/Kulibars',
 		},
 	},
 };
-
